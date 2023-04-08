52665b78db235b69e3ecefa6add0e17c3a4e068c
RobsonTrasel <robson.trasel@gmail.com>
2023-04-08 00:44:39 -0300

    feat(error): create appError class for errors

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,11 @@
+class AppError extends Error {
+  constructor(
+    public readonly message: string,
+    public readonly statusCode = 500,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export { AppError }
